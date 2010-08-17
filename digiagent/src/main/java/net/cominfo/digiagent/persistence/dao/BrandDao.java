5719ccd542d05b4a7d9a0cc629e543bb300f46f3
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-08-17 12:27:47 +0000

    Updated by porkin

--- digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/BrandDao.java
+++ digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/BrandDao.java
@@ -1,10 +1,21 @@
 package net.cominfo.digiagent.persistence.dao;
 
+import java.util.List;
+
+import net.cominfo.digiagent.persistence.domain.Brand;
 import net.cominfo.digiagent.persistence.sqlmapdao.BrandDAOImpl;
+import net.cominfo.digiagent.utils.Page;
 
 import org.springframework.stereotype.Repository;
  
 @Repository
 public class BrandDao extends BrandDAOImpl {
 
+	public Page<Brand> findPage(final Page<Brand> page) {	
+		List result = getSqlMapClientTemplate().queryForList("t_da_brand_Custom.pagebyId", page);
+		page.setTotalCount(result.size());
+		page.setResult(result);
+		return page;
+	}
+	
 }
