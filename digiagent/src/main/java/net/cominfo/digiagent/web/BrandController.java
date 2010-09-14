ae402573a3e85b3d7c9958570cbe603e3fd193b7
songjie831104@gmail.com <songjie831104@gmail.com@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-09-14 14:20:06 +0000

    Updated by songjie

--- digiagent/src/main/java/net/cominfo/digiagent/web/BrandController.java
+++ digiagent/src/main/java/net/cominfo/digiagent/web/BrandController.java
@@ -21,21 +21,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 @Controller
 @RequestMapping(value="/brand")
-@SessionAttributes("user")
 public class BrandController {
 	
 	@Autowired
 	private BrandService brandService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String getList(@ModelAttribute("user") Map user, Model model) {
-		String userName = (String)user.get("userName");
-		System.out.println(userName);
+	public String getList(Model model) {
 		return "brand";
 	}
 	
