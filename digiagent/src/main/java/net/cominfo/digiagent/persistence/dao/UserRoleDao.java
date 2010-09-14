c2c0712589219b382dd2d1ee276d7e7d920eda1c
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-09-14 19:01:56 +0000

    Updated by porkin

--- digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/UserRoleDao.java
+++ digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/UserRoleDao.java
@@ -1,9 +1,17 @@
 package net.cominfo.digiagent.persistence.dao;
 
+import java.util.List;
+
 import net.cominfo.digiagent.persistence.sqlmapdao.UserRoleDAOImpl;
 
 import org.springframework.stereotype.Repository;
- 
+
 @Repository
 public class UserRoleDao extends UserRoleDAOImpl {
+
+	@SuppressWarnings("unchecked")
+	public List<String> getRoleNameList(String name) {
+		return (List<String>) getSqlMapClientTemplate().queryForList(
+				"t_da_userrole_Custom.getRoleInfoByUsername");
+	}
 }
