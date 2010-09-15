6a90a70a4cdcebdf84276e0b3a4c51474cc58524
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-09-15 15:18:59 +0000

    Updated by porkin

---
+++ digiagent/src/main/java/net/cominfo/digiagent/web/SearchController.java
@@ -0,0 +1,23 @@
+package net.cominfo.digiagent.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@RequestMapping(value="/search")
+public class SearchController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String getView(Model model) {
+		Map user = new HashMap();
+		user.put("userName", "songjie");
+		model.addAttribute("user", user);
+		return "admin";
+	}
+}
