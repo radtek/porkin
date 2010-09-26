6c4020b4de1d70e2e038312036f928b79a581116
songjie831104@gmail.com <songjie831104@gmail.com@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-09-26 18:36:07 +0000

    Updated by songjie

---
+++ digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/CommodityImageDao.java
@@ -0,0 +1,10 @@
+package net.cominfo.digiagent.persistence.dao;
+
+import net.cominfo.digiagent.persistence.sqlmapdao.CommodityImageDAOImpl;
+
+import org.springframework.stereotype.Repository;
+ 
+@Repository
+public class CommodityImageDao extends CommodityImageDAOImpl {
+
+}
