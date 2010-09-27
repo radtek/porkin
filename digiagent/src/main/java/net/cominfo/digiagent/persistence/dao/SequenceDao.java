01e4fadf141b8613d08eb479df47006293b7e7f5
songjie831104@gmail.com <songjie831104@gmail.com@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-09-27 17:29:21 +0000

    Updated by songjie

--- digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/SequenceDao.java
+++ digiagent/src/main/java/net/cominfo/digiagent/persistence/dao/SequenceDao.java
@@ -12,6 +12,36 @@ public class SequenceDao extends SequenceDAOImpl {
 	
 	private final String USER_KEY = "user";
 
+	private final String COMMODITY_KEY = "commodity";
+	
+	private final String BRAND_KEY = "brand";
+	
+	private final String CATEGORY_KEY = "category";
+	
+	private final String CITY_KEY = "city";
+	
+	private final String COMMODITYIMAGE_KEY = "commodityimage";
+	
+	private final String CONTACT_KEY = "contact";
+	
+	private final String COUNTRY_KEY = "country";
+
+	private final String HISTORY_KEY = "history";
+	
+	private final String PRODUCT_KEY = "product";
+	
+	private final String PRODUCTBRAND_KEY = "productbrand";
+	
+	private final String PROVINCE_KEY = "province"; 
+	
+	private final String ROLE_KEY = "role";
+	
+	private final String SUPPLIER_KEY = "supplier";
+	
+	private final String SUPPLIERPRODUC_KEY = "supplierproduc";
+	
+	private final String USERROLE_KEY = "userrole";
+	
 	/**
 	 * This is a generic sequence ID generator that is based on a database table
 	 * called 'SEQUENCE', which contains two columns (NAME, NEXTID). This
@@ -42,4 +72,80 @@ public class SequenceDao extends SequenceDAOImpl {
 		return getNextId(USER_KEY);
 	}
 
+	public int getCommodityNexId(){
+		return getNextId(COMMODITY_KEY);
+	}
+
+
+
+	public int getBrandNexId() {
+		return getNextId(BRAND_KEY);
+	}
+
+
+	public int getCategoryNexId() {
+		return getNextId(CATEGORY_KEY);
+	}
+
+
+	public int getCityNexId() {
+		return getNextId(CITY_KEY);
+	}
+
+
+	public int getCommodityimageNexId() {
+		return getNextId(COMMODITYIMAGE_KEY);
+	}
+
+
+	public int getContactNexId() {
+		return getNextId(CONTACT_KEY);
+	}
+
+
+	public int getCountryNexId() {
+		return getNextId(COUNTRY_KEY);
+	}
+
+
+	public int getHistoryNexId() {
+		return getNextId(HISTORY_KEY);
+	}
+
+
+	public int getProductNexId() {
+		return getNextId(PRODUCT_KEY);
+	}
+
+
+	public int getProductbrandNexId() {
+		return getNextId(PRODUCTBRAND_KEY);
+	}
+
+
+	public int getProvinceNexId() {
+		return getNextId(PROVINCE_KEY);
+	}
+
+
+	public int getRoleNexId() {
+		return getNextId(ROLE_KEY);
+	}
+
+
+	public int getSupplierNexId() {
+		return getNextId(SUPPLIER_KEY);
+	}
+
+
+	public int getSupplierproducNexId() {
+		return getNextId(SUPPLIERPRODUC_KEY);
+	}
+
+
+	public int getUserroleNexId() {
+		return getNextId(USERROLE_KEY);
+	}
+	
+	
 }
