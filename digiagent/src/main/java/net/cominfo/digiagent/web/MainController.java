1ead6ebc972ae016e3125f8779126b8b470d438b
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-10-11 13:38:55 +0000

    Updated by porkin

--- digiagent/src/main/java/net/cominfo/digiagent/web/MainController.java
+++ digiagent/src/main/java/net/cominfo/digiagent/web/MainController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping(value="/admin")
-@SessionAttributes("user")
+@SessionAttributes({"userId","userName"})
 public class MainController {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String getView(Model model) {
+	public String getView(@ModelAttribute("userName") String userName,Model model) {
 		Map user = new HashMap();
-		user.put("userName", "songjie");
+		user.put("userName", userName);
 		model.addAttribute("user", user);
 		return "admin";
 	}
