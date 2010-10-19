23cedb908ac730ac5afea01d34814de7dd9a8044
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-10-19 13:24:43 +0000

    Updated by porkin

--- digiagent/src/main/java/net/cominfo/digiagent/web/ReportController.java
+++ digiagent/src/main/java/net/cominfo/digiagent/web/ReportController.java
@@ -17,42 +17,86 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/report")
 public class ReportController {
 
-	@RequestMapping(value = "/demo",method = RequestMethod.GET)
-	public ModelAndView  display(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	@RequestMapping(value = "/everyday", method = RequestMethod.GET)
+	public ModelAndView everyday(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		response.setContentType("image/jpeg");
-		response.setCharacterEncoding("utf-8");  
+		response.setCharacterEncoding("utf-8");
 		DefaultPieDataset data = getDataSet();
-		//JFreeChart chart = null;
-		JFreeChart chart = ChartFactory.createPieChart(
-	            "水果产量图",  // chart title
-	            data,             // data
-	            true,               // include legend
-	            true,
-	            false
-	        );
-		
+		// JFreeChart chart = null;
+		JFreeChart chart = ChartFactory.createPieChart("水果产量图", // chart title
+				data, // data
+				true, // include legend
+				true, false);
+
 		PiePlot plot = (PiePlot) chart.getPlot();
-        plot.setNoDataMessage("No data available");
-        plot.setCircular(false);
-        plot.setLabelGap(0.02);
-        
+		plot.setNoDataMessage("No data available");
+		plot.setCircular(false);
+		plot.setLabelGap(0.02);
+
 		ChartUtilities.writeChartAsJPEG(response.getOutputStream(), 1, chart,
 				400, 300, null);
-		
+
+		return null;
+
+	}
+
+	@RequestMapping(value = "/weekly", method = RequestMethod.GET)
+	public ModelAndView weekly(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		response.setContentType("image/jpeg");
+		response.setCharacterEncoding("utf-8");
+		DefaultPieDataset data = getDataSet();
+		// JFreeChart chart = null;
+		JFreeChart chart = ChartFactory.createPieChart("水果产量图", // chart title
+				data, // data
+				true, // include legend
+				true, false);
+
+		PiePlot plot = (PiePlot) chart.getPlot();
+		plot.setNoDataMessage("No data available");
+		plot.setCircular(false);
+		plot.setLabelGap(0.02);
+
+		ChartUtilities.writeChartAsJPEG(response.getOutputStream(), 1, chart,
+				400, 300, null);
+
+		return null;
+
+	}
+
+	@RequestMapping(value = "/monthly", method = RequestMethod.GET)
+	public ModelAndView monthly(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		response.setContentType("image/jpeg");
+		response.setCharacterEncoding("utf-8");
+		DefaultPieDataset data = getDataSet();
+		// JFreeChart chart = null;
+		JFreeChart chart = ChartFactory.createPieChart("水果产量图", // chart title
+				data, // data
+				true, // include legend
+				true, false);
+
+		PiePlot plot = (PiePlot) chart.getPlot();
+		plot.setNoDataMessage("No data available");
+		plot.setCircular(false);
+		plot.setLabelGap(0.02);
+
+		ChartUtilities.writeChartAsJPEG(response.getOutputStream(), 1, chart,
+				400, 300, null);
+
 		return null;
 
 	}
 
 	private static DefaultPieDataset getDataSet() {
 		DefaultPieDataset dataset = new DefaultPieDataset();
-		dataset.setValue("苹果",100);
-		dataset.setValue("梨子",200);
-		dataset.setValue("葡萄",300);
-		dataset.setValue("香蕉",400);
-		dataset.setValue("荔枝",500);
+		dataset.setValue("苹果", 100);
+		dataset.setValue("梨子", 200);
+		dataset.setValue("葡萄", 300);
+		dataset.setValue("香蕉", 400);
+		dataset.setValue("荔枝", 500);
 		return dataset;
 	}
-
 
 }
