d16a052acab29fcf2339c01a2e636940988c09f6
sleeplessor <sleeplessor@0dc3362d-3a98-f351-2b7f-563e44965cdb>
2010-12-19 17:29:16 +0000

    Updated by porkin

--- digiagent/src/main/java/net/cominfo/digiagent/web/MainController.java
+++ digiagent/src/main/java/net/cominfo/digiagent/web/MainController.java
@@ -3,6 +3,9 @@ package net.cominfo.digiagent.web;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.cominfo.digiagent.service.SortableService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes({"userId","userName"})
 public class MainController {
 	
+	@Autowired
+	private SortableService sortableService;
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method=RequestMethod.GET)
@@ -24,5 +29,11 @@ public class MainController {
 		user.put("userName", userName);
 		model.addAttribute("user", user);
 		return "admin";
+	}
+	
+	@RequestMapping(value = "/reset", method = RequestMethod.GET)
+	public String reset(@ModelAttribute("userName") String userName,Model model) {
+		sortableService.reset();
+		return null;
 	}
 }
